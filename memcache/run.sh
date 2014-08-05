ac311f43aac3666e836139e8dd193d1769f64b98
Joshua Cranmer <Pidgeot18@gmail.com>
2014-08-05 10:44:04 -0500

    Add some tuning parameters to ipcopter-scripts

--- memcache/run.sh
+++ memcache/run.sh
@@ -2,5 +2,10 @@
 
 set -e
 pushd $HOME/memcachetest &>/dev/null
-./memcachetest -h 127.0.0.1:11211 -t 1 -P 33 -i 10000 -c 200000 -l -T 60
+for nthreads in $(seq 1 32); do
+  for prob in 0 10 20 25 33 50 67 75 80 90 100; do
+    echo $nthreads $prob
+    ./memcachetest -h 127.0.0.1:11211 -t $nthreads -P $prob -i 10000 -c 200000 -l -T 60
+  done
+done
 popd &>/dev/null
--- netperf/run.sh
+++ netperf/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 set -e
-netperf -t TCP_STREAM -l 90
+
+for size in $(python -c "print ' '.join(map(lambda x:str(2**x),range(21)))"); do
+  netperf -t TCP_STREAM -l 10 -fM -I 95,5 -i 3,30 -- -m $size
+done
