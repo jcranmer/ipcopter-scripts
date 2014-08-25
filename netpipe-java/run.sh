bae04b202798e46d9a8c3832d1a756f3430b2005
Will Dietz <w@wdtz.org>
2014-08-25 15:48:30 -0500

    netpipe-java/*.sh: Fix execution from other directories than containing.

--- netpipe-java/run.sh
+++ netpipe-java/run.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-pushd netpipe-Java-1.0 &> /dev/null
+ROOT=$(readlink -f $(dirname $0))
+
+pushd $ROOT/netpipe-Java-1.0 &> /dev/null
 
 java Netpipe TCP -r &
 java Netpipe TCP -t -h localhost -o np.out -P -u 16777216
--- netpipe-java/setup.sh
+++ netpipe-java/setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+set -e
+
+ROOT=$(readlink -f $(dirname $0))
+
+pushd $ROOT &> /dev/null
+
 tar xvf netpipe-Java-1.0.tar.gz
 cd netpipe-Java-1.0
 make
+
+popd &> /dev/null
