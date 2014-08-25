0f4d17ede2c3f17c29a658a99336a0f17a7b7c6b
Will Dietz <w@wdtz.org>
2014-08-25 16:29:50 -0500

    install/uninstall scripts need priv, sudo for them

--- config-setup.sh
+++ config-setup.sh
@@ -4,10 +4,10 @@ ROOT=$(readlink -f $(dirname $0))
 
 case $CONFIG in
 ipcd)
-  $ROOT/install-ipcd.sh
+  sudo $ROOT/install-ipcd.sh
   ;;
 baseline)
-  $ROOT/uninstall-ipcd.sh
+  sudo $ROOT/uninstall-ipcd.sh
   ;;
 *)
   echo "Unrecognized configuration: CONFIG='$CONFIG'"
