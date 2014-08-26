6d3284de26bcb7b6313746ee9068d3b583890c6d
Will Dietz <w@wdtz.org>
2014-08-26 10:47:29 -0500

    'git pull' to ensure latest ipcopter, restart if started.

--- install-ipcd.sh
+++ install-ipcd.sh
@@ -4,6 +4,9 @@ set -e
 IPCOPTER_DIR=/home/cranmer2/ipcopter
 
 # Build ipcd and make sure it's up-to-date first.
+pushd $IPCOPTER_DIR
+sudo -u cranmer2 git pull
+popd
 pushd $IPCOPTER_DIR/libipc
 sudo -u cranmer2 make
 popd
@@ -14,4 +17,4 @@ popd
 install $IPCOPTER_DIR/ipcd/ipcd /bin/ipcd
 install $IPCOPTER_DIR/libipc/libipc.so /lib/libipc.so
 #echo "/lib/libipc.so" > /etc/ld.so.preload
-initctl start ipcd
+initctl restart ipcd
