7def0f9bf017dfdfb6860bc2eac53435ce8ef13a
Will Dietz <w@wdtz.org>
2014-08-27 14:11:36 -0500

    netpipe: Generate results.csv!

--- netpipe-java/run.sh
+++ netpipe-java/run.sh
@@ -10,14 +10,16 @@ if [ $WORKLOAD == "test" ]; then
   MAX_CHUNK_SIZE=4
 fi
 
+RESULTS=$PWD/results
+rm -rf $RESULTS || true
+mkdir -p $RESULTS
+
 pushd $ROOT/netpipe-Java-1.0 &> /dev/null
 
 java Netpipe TCP -r -u $MAX_CHUNK_SIZE &
-java Netpipe TCP -t -h localhost -o np.out -P -u $MAX_CHUNK_SIZE
+java Netpipe TCP -t -h localhost -o $RESULTS/np.out -P -u $MAX_CHUNK_SIZE
 
-echo "======================="
-echo "Parse-able Results:"
-echo "======================="
-cat np.out
+echo '"Time","Throughput (bps)","Bits","Bytes","Variance"' > $RESULTS/results.csv
+awk '{$1=$1}1' OFS=',' < np.out >> $RESULTS/results.csv
 
 popd &> /dev/null
