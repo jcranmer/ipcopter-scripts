d8e1ce7a4ad74cfdbedc44611037291114144388
Will Dietz <w@wdtz.org>
2014-08-27 14:23:45 -0500

    memcache: Put results into results.csv, untested locally.

--- memcache/run.sh
+++ memcache/run.sh
@@ -14,11 +14,27 @@ if [ "$WORKLOAD" == "test" ]; then
   PROBS="50"
 fi
 
+RESULTS=$PWD/results
+rm -rf $RESULTS || true
+mkdir -p $RESULTS
+
 pushd $HOME/memcachetest &>/dev/null
 for nthreads in $THREADS; do
   for prob in $PROBS; do
     echo $nthreads $prob
-    ./memcachetest -h 127.0.0.1:11211 -t $nthreads -P $prob -i $ITERS -c $COUNT -l -T $TIME
+    LOG=$RESULTS/${nthreads}.${prob}.log
+    stdbuf -oL -eL ./memcachetest -h 127.0.0.1:11211 -t $nthreads -P $prob -i $ITERS -c $COUNT -l -T $TIME |& tee $LOG
   done
 done
 popd &>/dev/null
+
+# Parse results into csv
+# TODO: Maybe make use of latency statistics? (How?)
+RESULTS_CSV=$RESULTS/results.csv
+echo '"Gets","Sets","Time","Ops/Sec"' > $RESULTS_CSV
+for nthreads in $THREADS; do
+  for prob in $PROBS; do
+    LOG=$RESULTS/${nthreads}.${prob}.log
+    tail -n1 $LOG | sed 's/[^0-9]*://g'|awk '{$1=$1}1' OFS="," >> $RESULTS_CSV
+  done
+done
