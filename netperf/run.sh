ac58172668e11c9103f57759b9ef19e6026f7d2b
Will Dietz <w@wdtz.org>
2014-08-27 14:03:23 -0500

    netperf: Gather results into csv for processing.
    Perhaps standardizing on putting results in 'results/results.csv'

--- netperf/run.sh
+++ netperf/run.sh
@@ -13,6 +13,20 @@ if [ $WORKLOAD == "test" ]; then
   TIME=5
 fi
 
+RESULTS=$PWD/results
+rm -rf $RESULTS || /bin/true
+mkdir -p $RESULTS
+
 for size in $SIZES; do
-  netperf -t TCP_STREAM -l $TIME -fM -I $CONFID -i $ITERS -- -m $size
+  LOG=$RESULTS/${size}.log
+  stdbuf -oL -eL netperf -t TCP_STREAM -l $TIME -fM -I $CONFID -i $ITERS -- -m $size |& tee $LOG
+done
+
+RESULT_CSV=$RESULTS/results.csv
+
+echo '"Recv Socket Size","Send Socket Size","Send Message Size", "Elapsed Time", "Throughput MB/s"' > $RESULT_CSV
+
+for size in $SIZES; do
+  LOG=$RESULTS/${size}.log
+  tail -n1 $LOG | awk '{$1=$1}1' OFS="," >> $RESULT_CSV
 done
