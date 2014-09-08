34eb354d1f1c6e72375ca224bf1cbeeb12f2122d
Will Dietz <w@wdtz.org>
2014-09-07 19:09:34 -0500

    Update to reflect new libipc logging location.

--- install-ipcd.sh
+++ install-ipcd.sh
@@ -20,7 +20,7 @@ install $IPCOPTER_DIR/libipc/libipc.so /lib/libipc.so
 
 # initctl stop ipcd || true
 # Remove all existing ipc(d) logs
-find /tmp/ -maxdepth 1 -type f -name "ipcd.*.log" -exec rm -f {} \;
+find /tmp/ipcd -maxdepth 1 -type f -name "*.log" -exec rm -f {} \;
 # Remove any existing shared memory segments as well
 find /dev/shm -maxdepth 1 -name "ipcd.*" -exec rm -f {} \;
 # initctl start ipcd || true
