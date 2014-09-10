3064eaa3798b5468301882d70e756969f7c12cb2
Will Dietz <w@wdtz.org>
2014-09-10 17:48:14 -0500

    pgbench: Try running with a few different scale factors.

--- postgresql/run.sh
+++ postgresql/run.sh
@@ -2,22 +2,19 @@
 
 set -e
 set -x
-# Clear the database if it exists.
-dropdb --if-exists pgbench
-createdb pgbench
-
-SCALE_FACTOR=30
-TIME=600
 
 case $WORKLOAD in
 test)
+  SCALES="1"
   TIME=10
   ;;
 basic)
   # 1m
+  SCALES="1 31 61"
   TIME=60
   ;;
 full)
+  SCALES="1 11 21 31 41 51 61 71"
   # 10m
   TIME=600
   ;;
@@ -30,16 +27,24 @@ RESULTS=$PWD/results
 rm -rf $RESULTS || true
 mkdir -p $RESULTS
 
-# Run pgbench
-LOG=$RESULTS/s30.log
-pgbench -i -s $SCALE_FACTOR -h localhost pgbench |& tee $LOG
-pgbench -s $SCALE_FACTOR -T $TIME -r -h localhost pgbench |& tee -a $LOG
-
 # Create results csv
 RESULTS_CSV=$RESULTS/results.csv
 
-# For now, just the two TPS values.
-# TODO: Other parameters?
-# TODO: Select-only?
-# TODO: Latency numbers?
-grep tps $LOG | sed 's/^tps = \([0-9.]*\).*$/\1/' | tr '\n' ', ' |sed 's/,$/\n/' > $RESULTS_CSV
+echo '"TPS (incl)", "TPS (excl)"' > $RESULTS_CSV
+
+# Run pgbench
+for scale in $SCALES; do
+  LOG=$RESULTS/s${scale}.log
+  # Clear the database if it exists.
+  dropdb --if-exists pgbench
+  createdb pgbench
+
+  pgbench -i -s $scale -h localhost pgbench |& tee $LOG
+  pgbench -s $scale -T $TIME -r -h localhost pgbench |& tee -a $LOG
+
+
+  # TODO: Select-only?
+  # TODO: Latency numbers?
+  grep tps $LOG | sed 's/^tps = \([0-9.]*\).*$/\1/' | tr '\n' ', ' |sed 's/,$/\n/' | \
+    sed 's/^/$scale,/' >> $RESULTS_CSV
+done
