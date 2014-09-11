39c968b79389a9660399d152cab474f336a67df1
Will Dietz <w@wdtz.org>
2014-09-10 21:13:04 -0500

    postgresql: select-only, prefer iters over longer tests.
    Iterations seems to be the general wisdom to get
    reasonably reliable results from pgbench.
    
    Select-only is both default of various pgbench
    automation suites (pgbench-tools, etc.) and
    I believe makes the operations more network-bound.
    Maybe.

--- postgresql/run.sh
+++ postgresql/run.sh
@@ -6,17 +6,21 @@ set -x
 case $WORKLOAD in
 test)
   SCALES="1"
+  # 10s
   TIME=10
+  ITERS=1
   ;;
 basic)
-  # 2m
   SCALES="1 21 41 61"
-  TIME=120
+  # 1m
+  TIME=60
+  ITERS=3
   ;;
 full)
   SCALES="1 21 41 61"
-  # 10m
-  TIME=600
+  # 1m
+  TIME=60
+  ITERS=7
   ;;
 *)
   echo "Unrecognized WORKLOAD '$WORKLOAD'"
@@ -30,26 +34,30 @@ mkdir -p $RESULTS
 # Create results csv
 RESULTS_CSV=$RESULTS/results.csv
 
-echo '"Scale Factor","TPS (incl)", "TPS (excl)"' > $RESULTS_CSV
+echo '"Scale Factor","Iteration","TPS (incl)", "TPS (excl)"' > $RESULTS_CSV
 
 export PGUSER=postgres
 export PGHOST=localhost
 
 # Run pgbench
 for scale in $SCALES; do
-  LOG=$RESULTS/s${scale}.log
   # Clear the database if it exists.
   dropdb --if-exists pgbench
   createdb pgbench
 
-  pgbench -i -s $scale -h localhost pgbench |& tee $LOG
-  pgbench -s $scale -T $TIME -r -h localhost pgbench |& tee -a $LOG
+  # create test db once for each scale size
+  pgbench -i -s $scale -h localhost pgbench
+  # do multiple iterations with same db (??)
+  for i in $(seq $ITERS); do
+    LOG=$RESULTS/s${scale}.i${i}.log
+    pgbench -s $scale -S -T $TIME -r -h localhost pgbench |& tee $LOG
 
 
-  # TODO: Select-only?
-  # TODO: Latency numbers?
-  grep tps $LOG | sed 's/^tps = \([0-9.]*\).*$/\1/' | tr '\n' ', ' |sed 's/,$/\n/' | \
-    sed "s/^/$scale,/" >> $RESULTS_CSV
+    # TODO: Select-only?
+    # TODO: Latency numbers?
+    grep tps $LOG | sed 's/^tps = \([0-9.]*\).*$/\1/' | tr '\n' ', ' |sed 's/,$/\n/' | \
+      sed "s/^/$scale,$i/" >> $RESULTS_CSV
+  done
 done
 
 # Attempt to cleanly exit the server, if we get this far...
