b812e79edf25525ee8067067482acee806cbc700
Will Dietz <w@wdtz.org>
2014-09-17 18:32:45 -0500

    netpipe: Run multiple iterations, diff perturb for each.
    Update plot scripts too, untested.
    
    Ideally (?) the 'relative' comparison would be done
    against summaries of the two distrubtions, not
    pair-wise...
    
    But that can be improved upon later.

--- netpipe/run.sh
+++ netpipe/run.sh
@@ -7,10 +7,12 @@ ROOT=$(readlink -f $(dirname $0))
 case $WORKLOAD in
 test)
   MAX_CHUNK_SIZE=4
+  ITERS=1
   ;;
 full | basic)
   #SIZES= (default)
   MAX_CHUNK_SIZE=16777216
+  ITERS=3
   ;;
 *)
   echo "Unrecognized WORKLOAD '$WORKLOAD'"
@@ -23,18 +25,21 @@ mkdir -p $RESULTS
 
 pushd $ROOT/NetPIPE-3.7.1 &> /dev/null
 
-PORT=1
-while [ $PORT -lt 1024 ]; do
-  PORT=$RANDOM
+echo '"Iteration", "Perturbation", "Bytes","Throughput (Mbps)","Time (s)"' > $RESULTS/results.csv
+
+for i in $(seq $ITERS); do
+  PORT=1
+  while [ $PORT -lt 1024 ]; do
+    PORT=$RANDOM
+  done
+  echo $PORT
+  OUT=$RESULTS/np.${i}.out
+
+  ./NPtcp -l 1 -u $MAX_CHUNK_SIZE -P$PORT -p$i &
+  sleep 1
+  ./NPtcp -h localhost -o $OUT -l 1 -u $MAX_CHUNK_SIZE -P$PORT -p$i
+
+  awk '{$1=$1}1' OFS=',' < $OUT | sed "s/^/$i,$i,/" >> $RESULTS/results.csv
 done
-echo $PORT
-
-./NPtcp -l 1 -u $MAX_CHUNK_SIZE -P$PORT &
-sleep 1
-./NPtcp -h localhost -o $RESULTS/np.out -l 1 -u $MAX_CHUNK_SIZE -P$PORT
-
-# Shame that java version has better information?
-echo '"Bytes","Throughput (Mbps)","Time (s)"' > $RESULTS/results.csv
-awk '{$1=$1}1' OFS=',' < $RESULTS/np.out >> $RESULTS/results.csv
 
 popd &> /dev/null
