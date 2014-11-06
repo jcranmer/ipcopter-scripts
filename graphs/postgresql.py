42c082d136012a7f3fb2b1c10e58d110b3ae6dcf
Joshua Cranmer <Pidgeot18@gmail.com>
2014-11-06 15:53:08 -0600

    Add support for generating the postgresql graph using the new graphtools API.

---
+++ graphs/postgresql.py
@@ -0,0 +1,19 @@
+add_gnuplot_commands("""
+set grid
+
+set xlabel "Scale Factor"
+set ylabel "TPS"
+
+set style data histogram
+set boxwidth 0.4
+set style fill solid 0.5 border
+set yrange [0:110]
+set xtics 0,20
+""")
+
+ipcd = read_csv("postgresql.ipcd.csv", xcol=0)[2].mean()
+ipcd.title = "Slipstream"
+baseline = read_csv("postgresql.baseline.csv", xcol=0)[2].mean()
+baseline.title = "Baseline"
+
+plot([ipcd, baseline])
